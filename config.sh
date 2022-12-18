10f76254cc838f3eb21a9e42b924f3bb570b8313
damblatt <dam.blatt@gmail.com>
2022-12-18 11:56:46 +0100

    commit

--- config.sh
+++ config.sh
@@ -2,5 +2,5 @@ firstBucketName="m347-testbucketnr1"
 secondBucketName="m347-testbucketnr2"
 functionName="m347-function"
 runtime="python3.9"
-roleARN= aws iam get-role --role-name $roleName --query 'Role.[RoleName, Arn]' --output text #funktioniert noch nicht
+roleARN="arn:aws:iam::341124230193:role/LabRole"
 # lambdaPolicySource="lambdaPolicy.json"
