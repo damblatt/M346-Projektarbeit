2de6ee53d23922af6a6b0df3cc8eb6d77fcb5960
damblatt <dam.blatt@gmail.com>
2022-12-18 13:01:29 +0100

    commit

--- config.sh
+++ config.sh
@@ -4,6 +4,7 @@ firstBucketName="m347-testbucketnr1"
 secondBucketName="m347-testbucketnr2"
 functionName="m347-function"
 runtime="python3.9"
+handler="main.handler"
+zip zipFile function.js # creates zip-file containing function.js
 roleARN=$(aws iam get-role --role-name LabRole | grep Arn | awk -F '"' '{print $4}') # Gets role named 'LabRole' and filters the output to get the Arn
 # lambdaPolicySource="lambdaPolicy.json"
-zip zipFile function.js # creates zip-file containing function.js
--- createLambda.sh
+++ createLambda.sh
@@ -5,5 +5,6 @@ source config.sh
 aws lambda create-function \
     --function-name $functionName \
     --runtime $runtime \
+    --handler $handler \
     --zip-file fileb://zipFile.zip \
     --role $roleARN
