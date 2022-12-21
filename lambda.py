8cfed2683c1edcc9e2f5e41faabc09b77507fb60
mabruelisauer <112378881+mabruelisauer@users.noreply.github.com>
2022-12-21 21:27:25 +0100

    worked on Lambda function

--- lambda.py
+++ lambda.py
@@ -1,24 +1,14 @@
 import boto3
-import os
 from PIL import Image
-import pathlib
-from io import BytesIO
 
+s3 = boto3.client('s3')
 
-
-s3 = boto3.resource('s3')
-
-
-
-
-
-def create_this_bucket(name, location):
-    try:
-        s3.create_bucket(
-            Bucket=name,
-            CreateBucketConfiguration={
-                'LocationConstraint': location
-            }
-        )
-    except Exception as e:
-        print(e)
+def resize_image(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    response = s3.get_object(Bucket=bucket, Key=key)
+    image_content = response['Body'].read()
+    image = Image.open(BytesIO(image_content))
+    image.thumbnail((128, 128))
+    
+    s3.put_object(Bucket='target-bucket', Key=key, Body=image)
