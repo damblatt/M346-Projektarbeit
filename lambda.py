eb7d370c107b3ace5154d544fc6f045e5a698e96
Maurin Inauen <Maurin.Inauen@ksb-sg.ch>
2022-12-21 21:27:02 +0100

    python sowas

--- lambda.py
+++ lambda.py
@@ -4,21 +4,14 @@ from PIL import Image
 import pathlib
 from io import BytesIO
 
+s3 = boto3.client('s3')
 
+def resize_image(event):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    response = s3.get_object(Bucket=bucket, Key=key)
+    image_content = response['Body'].read()
+    image = Image.open(BytesIO(image_content))
+    image.thumbnail((128, 128))
 
-s3 = boto3.resource('s3')
-
-
-
-
-
-def create_this_bucket(name, location):
-    try:
-        s3.create_bucket(
-            Bucket=name,
-            CreateBucketConfiguration={
-                'LocationConstraint': location
-            }
-        )
-    except Exception as e:
-        print(e)
+    s3.put_object(Bucket='target-bucket', Key=key, Body=image)
