f8b2eec5290fac9914d465cc5486e88ee5135330
mabruelisauer <112378881+mabruelisauer@users.noreply.github.com>
2022-12-23 21:41:00 +0100

    trigger

--- createLambda.sh
+++ createLambda.sh
@@ -2,6 +2,7 @@
 
 source config.sh
 # source createRole.sh
+# create lambda function
 aws lambda create-function \
     --function-name $functionName \
     --runtime $runtime \
@@ -9,8 +10,9 @@ aws lambda create-function \
     --zip-file fileb://zipFile.zip \
     --role $roleARN
 
-echo "> Preparing lambda function..."
-zip func.zip testlambda.py > /dev/null
-
-echo "> Creating lambda function..."
-aws lambda create-function --function-name $functionName --zip-file fileb://func.zip --handler $handler --runtime python.x --role $roleARN > /dev/null
+# create S3-Trigger
+aws lambda create-event-source-mapping \
+  --function-name $functionName \
+  --event-source-arn arn:aws:s3:::$firstBucketName \
+  --batch-size 1 \
+  --starting-position "LATEST"
